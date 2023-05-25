36755e2dfbff6ab4855113f6c13876aff0fc798d
khoozhaowei2002 <khoozhaowei2002@outlook.my>
2023-05-25 11:16:53 +0800

    done

--- download.sh
+++ download.sh
@@ -1,9 +1,9 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # This software may be used and distributed according to the terms of the GNU General Public License version 3.
 
-PRESIGNED_URL=""             # replace with presigned url from email
+PRESIGNED_URL="https://dobf1k6cxlizq.cloudfront.net/*?Policy=eyJTdGF0ZW1lbnQiOlt7IlJlc291cmNlIjoiaHR0cHM6Ly9kb2JmMWs2Y3hsaXpxLmNsb3VkZnJvbnQubmV0LyoiLCJDb25kaXRpb24iOnsiRGF0ZUxlc3NUaGFuIjp7IkFXUzpFcG9jaFRpbWUiOjE2ODU0NjEyNjV9fX1dfQ__&Signature=Wxh84TFZG0prHfBkCXABXrfhEk1~633BBNtEC9IYVu~u8FvFYvXhIMTj5szfjt9WpcNPtNntoRXXAsJH-qyi4grvfHHb3WhnquvgDSoOA8Bh7wjUn9Afl0vY7SeBtqgXpHHUPer~fjlz134-Z-3WIH1bENaY9~s0DlnPQy-JbE7d9OcfHqIzJXNEk7hq-lHRu4-1lgub7MHpFEJm4EFOUFimaVv-UvFpyhAed~qBtha82iXX7daiLmFQfYC1A7fPYyhLZJadgIq~2zKFJwRN9rr3AY3xRHeB4FK45QAKC9CawgsS~HHs7yWqNK8WKNGcpwDGORq0ru0czfRFaVZGLA__&Key-Pair-Id=K231VYXPC1TA1R"             # replace with presigned url from email
 MODEL_SIZE="7B,13B,30B,65B"  # edit this list with the model sizes you wish to download
-TARGET_FOLDER=""             # where all files should end up
+TARGET_FOLDER=".."             # where all files should end up
 
 declare -A N_SHARD_DICT
 
